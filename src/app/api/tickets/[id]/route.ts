d0e8f01e3f4999dafcf3b68694b0f898169ceaa0
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-08-19 23:18:03 +0100

    well managed for this basic application

--- src/app/api/tickets/[id]/route.ts
+++ src/app/api/tickets/[id]/route.ts
@@ -1,15 +1,11 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Ticket from "@/models/ticketModel";
 
-interface ticketData {
-  name: string;
-  content: string;
-  severity: "low" | "medium" | "high";
-  owner: string;
-}
-
-export async function GET(req: Request, { params }) {
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   await dbConnect();
   const { id } = await params;
   const ticket = await Ticket.findById(id);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
     <div className="font-sans w-full">
       <div className="flex flex-row w-3/4 gap-2">
         <RectangleSelection text="Create Ticket" direction="create" />
-        <RectangleSelection text="Track Ticket" direction="track" />
         <RectangleSelection text="List Tickets" direction="list" />
       </div>
     </div>
